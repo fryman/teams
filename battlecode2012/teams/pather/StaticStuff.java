b1f2c100203db857ecb7d8ecc2e1d3320cf4f5eb
Stephen Freiberg <safreiberg@gmail.com>
2012-01-13 15:11:11 -0500

    removed some pathernav stuff

--- battlecode2012/teams/pather/StaticStuff.java
+++ battlecode2012/teams/pather/StaticStuff.java
@@ -2,17 +2,13 @@ package pather;
 
 import battlecode.common.*;
 
-import patherNav.BugNav;
-import patherNav.NavGeneral;
 
 public abstract class StaticStuff {
 	public static RobotController myRC;
 	public static MapLocation base;
-	public static NavGeneral myNav;
 	
 	public static void init(RobotController rc){
 		myRC = rc;
 		base = myRC.sensePowerCore().getLocation();
-		myNav = new BugNav();
 	}
 }
