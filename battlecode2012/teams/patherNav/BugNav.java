78905df0757db6f3c3f94a0bcb789ce59094c7eb
Stephen Freiberg <safreiberg@gmail.com>
2012-01-14 14:10:15 -0500

    added bugnav file (blank)

---
+++ battlecode2012/teams/patherNav/BugNav.java
@@ -0,0 +1,9 @@
+package patherNav;
+
+public class BugNav {
+/*
+ * This class will provide the methods necessary for bug navigation.
+ * Walk toward the target until obstructed, then follow the wall 
+ * until it is possible to approach the target again.
+ */
+}
