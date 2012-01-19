49d3660628a67c533c1cf9e111ba7cd1cc130bb3
brian <brian@ubuntu.ubuntu-domain>
2012-01-19 15:21:28 -0500

    Scout no longer shoot at units without flux.

--- battlecode2012/teams/team111/ScoutPlayer.java
+++ battlecode2012/teams/team111/ScoutPlayer.java
@@ -37,7 +37,7 @@ public class ScoutPlayer extends BasePlayer {
 		try {
 			//broadcastMessage();
 			Robot closestTar = senseClosestEnemy();
-			if (closestTar != null && myRC.senseRobotInfo(closestTar).type != RobotType.TOWER) {
+			if (closestTar != null && myRC.senseRobotInfo(closestTar).type != RobotType.TOWER && myRC.senseRobotInfo(closestTar).flux>.5) {
 				MapLocation Location = myRC.senseLocationOf(closestTar);
 				if (myRC.canAttackSquare(Location) && !myRC.isAttackActive()) {
 					myRC.setIndicatorString(0, "Attacking at the end of the turn.");
